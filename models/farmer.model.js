ba028e167e50173d716ad4771f53bff2861af807
vivek <vivekc.apk@gmail.com>
2024-11-25 01:48:47 +0530

    Order API

--- models/farmer.model.js
+++ models/farmer.model.js
@@ -3,23 +3,27 @@ import mongoose, { Schema, model } from "mongoose";
 const farmerSchema = new Schema({
   name: {
     type: String,
-    required: [true, "Farmer name requried"],
+    required: [true, "Farmer name required"],
   },
   village: {
     type: String,
-    required: [true, "Village name requried"],
+    required: [true, "Village name required"],
+  },
+  villageID: {
+    type: Schema.Types.ObjectId, // MongoDB ObjectId
+    required: [true, "Village ID required"],
   },
   taluka: {
     type: String,
-    required: [true, "Taluka name requried"],
+    required: [true, "Taluka name required"],
   },
   district: {
     type: String,
-    required: [true, "District name requried"],
+    required: [true, "District name required"],
   },
   mobileNumber: {
     type: Number,
-    required: [true, "Mobile number requried"],
+    required: [true, "Mobile number required"],
     unique: true,
   },
 });
