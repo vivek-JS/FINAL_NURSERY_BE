c6dfa10e02fd3e5d487bcd44cb2448d059b9475e
vivek <vivekc.apk@gmail.com>
2025-01-02 00:46:03 +0530

    Order API

--- models/order.model.js
+++ models/order.model.js
@@ -97,7 +97,15 @@ const orderSchema = new Schema(
     },
     farmReadyDate:{
       type: Date
-    }
+    },
+    returnedPlants: {
+      type: Number,
+      default: 0  // Default to 0 returned plants
+    },
+    returnReason: {
+      type: String,
+      // Only required if there are returned plants
+    },
   },
   { timestamps: true }
 );
