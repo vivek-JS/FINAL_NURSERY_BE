12da1a0f3942f563a52478737838e145c03331be
vivek <vivekc.apk@gmail.com>
2025-01-04 01:30:56 +0530

    Order API

--- models/plantOutward.model.js
+++ models/plantOutward.model.js
@@ -1,54 +1,98 @@
 import { Schema, model } from "mongoose";
 
 const labSchema = new Schema({
- outwardDate: {
-   type: Date,
-   required: true,
-   default: Date.now
- },
- size: {
-   type: String,
-   required: true,
-   enum: ['R1', 'R2', 'R3']
-},
- bottles: {
-   type: Number,
-   required: true,
-   min: 1
- }, 
- plants: {
-   type: Number,
-   required: true,
-   min: 1
- },
- rootingDate: {
-   type: Date,
-   required: true
- }
+  outwardDate: {
+    type: Date,
+    required: true,
+    default: Date.now
+  },
+  size: {
+    type: String,
+    required: true,
+    enum: ['R1', 'R2', 'R3']
+  },
+  bottles: {
+    type: Number,
+    required: true,
+    min: 1
+  }, 
+  plants: {
+    type: Number,
+    required: true,
+    min: 1
+  },
+  rootingDate: {
+    type: Date,
+    required: true
+  }
 });
 
 const outwardSchema = new Schema({
- labs: [labSchema]
+  labs: [labSchema]
+});
+
+// New primaryInward schema
+const primaryInwardSchema = new Schema({
+  primaryInwardDate: {
+    type: Date,
+    required: true
+  },
+  numberOfBottles: {
+    type: Number,
+    required: true,
+    min: 1
+  },
+  size: {
+    type: String,
+    required: true,
+    enum: ['R1', 'R2', 'R3']
+  },
+  cavity: {
+    type: Number,
+    required: true,
+    min: 1
+  },
+  numberOfTrays: {
+    type: Number,
+    required: true,
+    min: 1
+  },
+  totalQuantity: {
+    type: Number,
+    required: true,
+    min: 1
+  },
+  pollyhouse: {
+    type: String,
+    required: true
+  },
+  laboursEngaged: {
+    type: Number,
+    required: true,
+    min: 1
+  }
 });
 
 const plantOutwardSchema = new Schema({
-    batchId: {
-        type: Schema.Types.ObjectId,
-        ref: 'Batch',
-        required: true
-      },
- dateAdded: {
-   type: Date,
-   required: true,
-   default: Date.now
- },
- outward: [outwardSchema],
- isActive: {
-   type: Boolean,
-   default: true
- }
+  batchId: {
+    type: Schema.Types.ObjectId,
+    ref: 'Batch',
+    required: true
+  },
+  dateAdded: {
+    type: Date,
+    required: true,
+    default: Date.now
+  },
+  // Added primaryInward array field
+  primaryInward: [primaryInwardSchema],
+  outward: [outwardSchema],
+  isActive: {
+    type: Boolean,
+    default: true
+  }
 }, {
- timestamps: true
+  timestamps: true
 });
 
 const PlantOutward = model("PlantOutward", plantOutwardSchema);
--- routes/plantOutward.route.js
+++ routes/plantOutward.route.js
@@ -2,7 +2,11 @@ import express from "express";
 import {
  addLabEntry,
  updateLabEntry,
- getAllPlantOutwards
+ getAllPlantOutwards,
+ addPrimaryInward,
+ updatePrimaryInward,
+ deletePrimaryInward,
+ getPrimaryInwardByBatchId
 } from "../controllers/plantOutward.controller.js";
 
 const router = express.Router();
@@ -10,5 +14,8 @@ const router = express.Router();
 router.post("/batch/labs", addLabEntry); 
 router.put("/batch/outward/lab/:batchId/:outwardId/:labId", updateLabEntry);
 router.get("/outwards", getAllPlantOutwards);
-
+router.post('/plant-outward/primary-inward', addPrimaryInward);
+router.patch('/plant-outward/primary-inward/:batchId/:primaryInwardId', updatePrimaryInward);
+router.delete('/plant-outward/:batchId/primary-inward/:primaryInwardId', deletePrimaryInward);
+router.get('/plant-outward/primary-inward/:batchId', getPrimaryInwardByBatchId);
 export default router;
